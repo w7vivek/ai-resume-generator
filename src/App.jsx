fa3356937aa9accaa400fe2f4cacfc1b7d1702d6
w_vivek <wvivek.181@gmail.com>
2025-12-26 18:17:34 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // must be *react-router-dom*
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Index } from "./components/Index";
 import About from "./components/About";
 
 function App() {
   return (
     <Routes>
-        <Route path="/ai-resume-generator" element={<Index />} />
-        <Route path="/ai-resume-generator/about" element={<About />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
